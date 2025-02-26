ebe45bdb0558b3b3dcffe371161ae9e3003d64bd
yusuf <diandrayusuf23@gmail.com>
2025-02-26 22:15:15 +0700

    Update: Finish BackEnd

--- backend/controller/NoteController.js
+++ backend/controller/NoteController.js
@@ -20,3 +20,38 @@ export const createNote = async (req, res) => {
     console.log(error.message);
   }
 };
+
+export const updateNote = async (req, res) => {
+  try {
+    const inputData = req.body;
+    const id = req.params.id;
+
+    await Note.update(inputData, {
+      where: {
+        id,
+      },
+    });
+    res.status(200).json({
+      message: "Berhasil Update Data",
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const deleteNote = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Note.destroy({
+      where: {
+        id,
+      },
+    });
+    res.status(200).json({
+      message: "Berhasil Delete Data",
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- backend/routes/Route.js
+++ backend/routes/Route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getNotes, createNote } from "../controller/NoteController.js";
+import { getNotes, createNote, updateNote, deleteNote } from "../controller/NoteController.js";
 
 const router = express.Router();
 
 router.get('/notes', getNotes);
 router.post('/tambah-note', createNote);
+router.put('/update-note/:id', updateNote);
+router.delete('/delete-note/:id', deleteNote);
 
 export default router;
