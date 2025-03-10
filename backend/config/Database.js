b81508c5b472cd6d84159b0cf197e6ae29acc204
yusuf <diandrayusuf23@gmail.com>
2025-03-10 10:46:19 +0700

    ADD Database

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('note', 'root', '', {
-    host: 'localhost',
+const db = new Sequelize('notes-yusuf', 'root', '12345678', {
+    host: '35.226.218.71',
     dialect: 'mysql',
     timezone: '+07:00'
 })
