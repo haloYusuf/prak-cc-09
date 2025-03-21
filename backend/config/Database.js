a4021c207620ce91ac5be922bc723ee6a43da141
yusuf <diandrayusuf23@gmail.com>
2025-03-21 21:53:30 +0700

    Update: Tugas Docker Init

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('notes-yusuf', 'root', '12345678', {
-    host: '35.226.218.71',
+const db = new Sequelize('notes-yusuf', 'yusuf', '12345678', {
+    host: '34.135.33.30',
     dialect: 'mysql',
     timezone: '+07:00'
 })
