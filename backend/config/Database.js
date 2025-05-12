5cbdb90b614da95bf8893d475e325b0a283e225d
yusuf <diandrayusuf23@gmail.com>
2025-05-12 13:43:50 +0700

    Change IP

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
 const db = new Sequelize('notes-yusuf', 'yusuf', '12345678', {
-    host: '34.135.33.30',
+    host: '34.71.143.156',
     dialect: 'mysql',
     timezone: '+07:00'
 })
