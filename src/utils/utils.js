22bb5187de1eb01590c15a3b977d02bd51d00032
yusuf <diandrayusuf23@gmail.com>
2025-05-25 21:36:00 +0700

    Update: Change Base Url

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
 // export const BASE_URL =
 //   "https://backend-compe-363721261053.us-central1.run.app";
 
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://backend-compe-363721261053.us-central1.run.app";
